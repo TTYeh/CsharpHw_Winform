b7f900ec25fb958c049cac8dc252f926b0e162f3
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-01-19 22:27:58 +0800

    add hw04 ok

--- CsharpHw_Winform_Sol/HW_MyHomework/hw04_StudentStruc.Designer.cs
+++ CsharpHw_Winform_Sol/HW_MyHomework/hw04_StudentStruc.Designer.cs
@@ -28,12 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxChinese = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxEng = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxMath = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxDisplayMaxMin = new System.Windows.Forms.TextBox();
+            this.buttonCalMaxMinSuj = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.ForeColor = System.Drawing.Color.Black;
+            this.buttonDisplay.Location = new System.Drawing.Point(142, 242);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(112, 46);
+            this.buttonDisplay.TabIndex = 1;
+            this.buttonDisplay.Text = "顯示儲存內容";
+            this.buttonDisplay.UseVisualStyleBackColor = true;
+            this.buttonDisplay.Click += new System.EventHandler(this.buttonDisplay_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.ForeColor = System.Drawing.Color.Black;
+            this.buttonSave.Location = new System.Drawing.Point(24, 242);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(112, 46);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "暫時儲存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxName.ForeColor = System.Drawing.Color.Black;
+            this.textBoxName.Location = new System.Drawing.Point(88, 31);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(166, 31);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(20, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "姓名:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(20, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "國文:";
+            // 
+            // textBoxChinese
+            // 
+            this.textBoxChinese.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxChinese.ForeColor = System.Drawing.Color.Black;
+            this.textBoxChinese.Location = new System.Drawing.Point(88, 89);
+            this.textBoxChinese.Name = "textBoxChinese";
+            this.textBoxChinese.Size = new System.Drawing.Size(166, 31);
+            this.textBoxChinese.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(20, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "英文:";
+            // 
+            // textBoxEng
+            // 
+            this.textBoxEng.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxEng.ForeColor = System.Drawing.Color.Black;
+            this.textBoxEng.Location = new System.Drawing.Point(88, 147);
+            this.textBoxEng.Name = "textBoxEng";
+            this.textBoxEng.Size = new System.Drawing.Size(166, 31);
+            this.textBoxEng.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(20, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "數學:";
+            // 
+            // textBoxMath
+            // 
+            this.textBoxMath.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxMath.ForeColor = System.Drawing.Color.Black;
+            this.textBoxMath.Location = new System.Drawing.Point(88, 205);
+            this.textBoxMath.Name = "textBoxMath";
+            this.textBoxMath.Size = new System.Drawing.Size(166, 31);
+            this.textBoxMath.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.textBoxDisplayMaxMin);
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(260, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(278, 205);
+            this.panel1.TabIndex = 11;
+            // 
+            // textBoxDisplayMaxMin
+            // 
+            this.textBoxDisplayMaxMin.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxDisplayMaxMin.Enabled = false;
+            this.textBoxDisplayMaxMin.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxDisplayMaxMin.Location = new System.Drawing.Point(9, 142);
+            this.textBoxDisplayMaxMin.Multiline = true;
+            this.textBoxDisplayMaxMin.Name = "textBoxDisplayMaxMin";
+            this.textBoxDisplayMaxMin.Size = new System.Drawing.Size(266, 59);
+            this.textBoxDisplayMaxMin.TabIndex = 14;
+            // 
+            // buttonCalMaxMinSuj
+            // 
+            this.buttonCalMaxMinSuj.ForeColor = System.Drawing.Color.Black;
+            this.buttonCalMaxMinSuj.Location = new System.Drawing.Point(375, 242);
+            this.buttonCalMaxMinSuj.Name = "buttonCalMaxMinSuj";
+            this.buttonCalMaxMinSuj.Size = new System.Drawing.Size(163, 46);
+            this.buttonCalMaxMinSuj.TabIndex = 12;
+            this.buttonCalMaxMinSuj.Text = "最高分/最低分科目";
+            this.buttonCalMaxMinSuj.UseVisualStyleBackColor = true;
+            this.buttonCalMaxMinSuj.Click += new System.EventHandler(this.buttonCalMaxMinSuj_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(266, 132);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // hw04_StudentStruc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(556, 348);
+            this.Controls.Add(this.buttonCalMaxMinSuj);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxMath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxEng);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxChinese);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDisplay);
+            this.Name = "hw04_StudentStruc";
             this.Text = "hw04_StudentStruc";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonDisplay;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxChinese;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxEng;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxMath;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxDisplayMaxMin;
+        private System.Windows.Forms.Button buttonCalMaxMinSuj;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- CsharpHw_Winform_Sol/HW_MyHomework/hw04_StudentStruc.cs
+++ CsharpHw_Winform_Sol/HW_MyHomework/hw04_StudentStruc.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,150 @@ namespace HW_MyHomework
 {
     public partial class hw04_StudentStruc : Form
     {
+        private Student _stu;
         public hw04_StudentStruc()
         {
             InitializeComponent();
+            this.Load += Hw04_StudentStruc_Load;
         }
+
+        private void Hw04_StudentStruc_Load(object sender, EventArgs e)
+        {
+            _stu = new Student();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            _stu.Name = textBoxName.Text == string.Empty 
+                ? "無名氏" 
+                : textBoxName.Text;
+            int.TryParse(textBoxChinese.Text, out int chiScore);
+            int.TryParse(textBoxEng.Text, out int engScore);
+            int.TryParse(textBoxMath.Text, out int mathScore);
+
+            // ♪沒填成績就當零分啦♪~
+            _stu.ChineseScore= chiScore;
+            _stu.EngScore= engScore;
+            _stu.MathScore= mathScore;
+        }
+
+        private void buttonDisplay_Click(object sender, EventArgs e)
+        {
+            // 可以用繼承資料的模式，但這個不用詳見:https://learn.microsoft.com/zh-tw/dotnet/framework/data/adonet/creating-a-datatable-from-a-query-linq-to-dataset
+            var dt = new DataTable("StudentData");
+            dt.Columns.Add(new DataColumn("姓名"));
+            dt.Columns.Add(new DataColumn("科目"));
+            dt.Columns.Add(new DataColumn("分數"));
+            //dt.Columns.Add("Subject", typeof(string));
+            //dt.Columns.Add("Score", typeof(int));
+
+            var row1 = dt.NewRow();
+            row1["姓名"] = _stu.Name;
+            row1["科目"] = "國文";
+            row1["分數"] = _stu.ChineseScore;
+            dt.Rows.Add(row1);
+            var row2 = dt.NewRow();
+            row2["姓名"] = _stu.Name;
+            row2["科目"] = "英文";
+            row2["分數"] = _stu.EngScore;
+            dt.Rows.Add(row2);
+            var row3 = dt.NewRow();
+            row3["姓名"] = _stu.Name;
+            row3["科目"] = "數學";
+            row3["分數"] = _stu.MathScore;
+            dt.Rows.Add(row3);
+
+            dataGridView1.DataSource= dt;
+
+            //textBoxDisplayAll.Text = $"姓名: {_stu.Name} \r\n" +
+            //    $"中文: {_stu.ChineseScore} \r\n" +
+            //    $"英文: {_stu.EngScore} \r\n" +
+            //    $"數學: {_stu.MathScore} " ;
+        }
+
+        private void buttonCalMaxMinSuj_Click(object sender, EventArgs e)
+        {
+            //_stu.EngScore= 0;
+            //_stu.ChineseScore
+            //_stu.MathScore
+            textBoxDisplayMaxMin.Text = $"最高分的成績為: {_stu.GetMaxScoreSuj()}{_stu.GetMinScore()} \r\n" +
+                $"最低分的成績為:{_stu.GetMinScoreSuj()} {_stu.GetMaxScore()}";
+        }
+    }
+    public class PersonalSubject {
+        public string Subject { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class Student {
+
+        public string Name { get; set; }
+        public int ChineseScore { get; set; }
+        public int EngScore { get; set; }
+        public int MathScore { get; set; }
+        
+        public string GetMaxScoreSuj()
+        {
+            // LINQ算RANK，硬是不想用字典
+            PersonalSubject[] allSubject = new PersonalSubject[] {
+                new PersonalSubject{ Subject = "中文" , Score = ChineseScore},
+                new PersonalSubject{ Subject = "英文" , Score = EngScore},
+                new PersonalSubject{ Subject = "數學" , Score = MathScore},
+            };
+
+            Dictionary<int, int> numRanks = allSubject
+                .GroupBy(i => i.Score)
+                .OrderByDescending(g => g.Key)
+                .Select((g, index) => (num: g.Key, rank: index + 1))
+                .ToDictionary(x => x.rank, x => x.num);
+
+            var query = from data in allSubject
+                        where data.Score == numRanks[1]
+                        select data.Subject;
+            string result = string.Empty;
+            foreach (var item in query.ToArray())
+            {
+                result += item;
+            }
+
+            return result;
+        }
+
+        public string GetMinScoreSuj()
+        {
+            // LINQ算RANK
+            PersonalSubject[] allSubject = new PersonalSubject[] {
+                new PersonalSubject{ Subject = "中文" , Score = ChineseScore},
+                new PersonalSubject{ Subject = "英文" , Score = EngScore},
+                new PersonalSubject{ Subject = "數學" , Score = MathScore},
+            };
+
+            Dictionary<int, int> numRanks = allSubject
+                .GroupBy(i => i.Score)
+                .OrderBy(g => g.Key)
+                .Select((g, index) => (num: g.Key, rank: index + 1))
+                .ToDictionary(x => x.rank, x => x.num);
+
+            var query = from data in allSubject
+                        where data.Score == numRanks[1]
+                        select data.Subject;
+            string result = string.Empty;
+            foreach (var item in query.ToArray())
+            {
+                result += item;
+            }
+
+            return result;
+        }
+        public int GetMaxScore()
+        {
+            return new List<int> { this.ChineseScore, this.EngScore, this.MathScore }.Max();
+        }
+
+        public int GetMinScore()
+        {
+            return new List<int> { this.ChineseScore, this.EngScore, this.MathScore }.Min();
+        }
+
     }
 }
