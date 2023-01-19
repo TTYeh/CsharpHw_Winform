39a42b5f5c83ce453b9bb096b287b91bf915375d
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-01-20 00:30:18 +0800

    hw13小畫家 ok

--- CsharpHw_Winform_Sol/HW_MyHomework/hw13_小畫家.Designer.cs
+++ CsharpHw_Winform_Sol/HW_MyHomework/hw13_小畫家.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonChooseColor = new System.Windows.Forms.Button();
+            this.labelDispColor = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.labelDispPenWidth = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonChooseColor
+            // 
+            this.buttonChooseColor.ForeColor = System.Drawing.Color.Black;
+            this.buttonChooseColor.Location = new System.Drawing.Point(678, 12);
+            this.buttonChooseColor.Name = "buttonChooseColor";
+            this.buttonChooseColor.Size = new System.Drawing.Size(83, 30);
+            this.buttonChooseColor.TabIndex = 0;
+            this.buttonChooseColor.Text = "選擇顏色";
+            this.buttonChooseColor.UseVisualStyleBackColor = true;
+            this.buttonChooseColor.Click += new System.EventHandler(this.buttonChooseColor_Click);
+            // 
+            // labelDispColor
+            // 
+            this.labelDispColor.AutoSize = true;
+            this.labelDispColor.BackColor = System.Drawing.Color.Black;
+            this.labelDispColor.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelDispColor.Location = new System.Drawing.Point(678, 44);
+            this.labelDispColor.Name = "labelDispColor";
+            this.labelDispColor.Size = new System.Drawing.Size(120, 30);
+            this.labelDispColor.TabIndex = 1;
+            this.labelDispColor.Text = "PenColor";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(678, 94);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 172);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // labelDispPenWidth
+            // 
+            this.labelDispPenWidth.AutoSize = true;
+            this.labelDispPenWidth.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelDispPenWidth.ForeColor = System.Drawing.Color.Black;
+            this.labelDispPenWidth.Location = new System.Drawing.Point(683, 273);
+            this.labelDispPenWidth.Name = "labelDispPenWidth";
+            this.labelDispPenWidth.Size = new System.Drawing.Size(125, 28);
+            this.labelDispPenWidth.TabIndex = 3;
+            this.labelDispPenWidth.Text = "PenWidth";
+            // 
+            // hw13_SmallPrinter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelDispPenWidth);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.labelDispColor);
+            this.Controls.Add(this.buttonChooseColor);
+            this.Name = "hw13_SmallPrinter";
             this.Text = "hw13_小畫家";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.hw13_SmallPrinter_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.hw13_SmallPrinter_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.hw13_SmallPrinter_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonChooseColor;
+        private System.Windows.Forms.Label labelDispColor;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label labelDispPenWidth;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
--- CsharpHw_Winform_Sol/HW_MyHomework/hw13_小畫家.cs
+++ CsharpHw_Winform_Sol/HW_MyHomework/hw13_小畫家.cs
@@ -12,9 +12,60 @@ namespace HW_MyHomework
 {
     public partial class hw13_SmallPrinter : Form
     {
+        private Graphics g;
+        private Pen p;
+        private int x0, y0;
+
         public hw13_SmallPrinter()
         {
             InitializeComponent();
+            this.Load += Hw13_SmallPrinter_Load;
+        }
+
+        private void Hw13_SmallPrinter_Load(object sender, EventArgs e)
+        {
+            labelDispPenWidth.Text = trackBar1.Value.ToString();
+            this.g = this.CreateGraphics();
+            this.p = new Pen(Color.Black, trackBar1.Value);
+        }
+
+        private void hw13_SmallPrinter_MouseDown(object sender, MouseEventArgs e)
+        {
+            //isDrawing = true;
+            this.x0 = e.X;
+            this.y0 = e.Y;
+        }
+
+        private void hw13_SmallPrinter_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.g.DrawLine(this.p, x0, y0, e.X, e.Y);
+                x0 =e.X;
+                y0 = e.Y;
+            
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            this.p.Width = trackBar1.Value;
+            labelDispPenWidth.Text = trackBar1.Value.ToString();
+        }
+
+        private void buttonChooseColor_Click(object sender, EventArgs e)
+        {
+            bool flag = this.colorDialog1.ShowDialog() == DialogResult.OK;
+            if (flag)
+            {
+                this.p.Color = this.colorDialog1.Color;
+                labelDispColor.BackColor= this.colorDialog1.Color;
+            }
+        }
+
+        private void hw13_SmallPrinter_MouseUp(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
